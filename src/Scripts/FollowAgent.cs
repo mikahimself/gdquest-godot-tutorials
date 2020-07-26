8160a1ed0ed982b6db436a5f9c8228ccd72a32c1
mika <mika.laihanen@gmail.com>
2020-07-26 18:23:06 +0300

    Finish up part IV. Add follow the leader behaviour, add background and little tweaks.

--- src/Scripts/FollowAgent.cs
+++ src/Scripts/FollowAgent.cs
@@ -6,17 +6,28 @@ public class FollowAgent : KinematicBody2D
     Sprite Triangle;
     [Export]
     public float MaxSpeed = 500.0f;
+    [Export]
+    public NodePath TargetPath;
+    [Export]
+    public float FollowOffset = 100f;
+    [Export]
+    public float ArriveRadius = 200f;
+    [Export]
+    public float Mass = 5.0f;
+
     public const float DISTANCE_TRESHOLD = 3.0f;
     private Vector2 _velocity = Vector2.Zero;
     private Vector2 _targetGlobalPosition;
     [Signal]
     public delegate void StartedMoving();
     public bool startedMoving = false;
+    private Node2D _target;
 
     public override void _Ready()
     {
         Triangle = (Sprite)GetNode("TriangleSprite");
         SetPhysicsProcess(false);
+        _target = (Node2D)GetNode(TargetPath);
     }
 
     public override void _UnhandledInput(InputEvent @event)
@@ -29,6 +40,9 @@ public class FollowAgent : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
+        _targetGlobalPosition = _target.GlobalPosition;
+        float toTarget = GlobalPosition.DistanceTo(_targetGlobalPosition);
+
         if (startedMoving)
         {
             // Emit signal one frame after agent starts moving. This is because
@@ -37,17 +51,28 @@ public class FollowAgent : KinematicBody2D
             EmitSignal(nameof(StartedMoving));
             startedMoving = false;
         }
-        if (Input.IsActionPressed("click"))
-        {
-            _targetGlobalPosition = GetGlobalMousePosition();
-            startedMoving = true;
-        }
 
-        if (GlobalPosition.DistanceTo(_targetGlobalPosition) < DISTANCE_TRESHOLD)
+        if (toTarget < DISTANCE_TRESHOLD)
         {
             return;
         }
-        _velocity = Steering.Follow(_velocity, GlobalPosition, _targetGlobalPosition, MaxSpeed);
+
+        // OffsetTargetPosition is a position slightly behind the object that the agent follows.
+        // We get it by calculating the direction to the target (subtract agent's position from target's
+        // position and normalize the result), multiplying the direction with the follow offset,
+        // and subtracting that from the target's global position.
+        // We only use it if the distance to target is greater than the follow offset. Otherwise, 
+        // the offsetTargetPosition is set to the current position.
+        Vector2 offsetTargetPosition = toTarget > FollowOffset 
+                                        ? _targetGlobalPosition - (_targetGlobalPosition - GlobalPosition).Normalized() * FollowOffset 
+                                        : GlobalPosition;
+
+        _velocity = Steering.ArriveTo(_velocity,
+                                        GlobalPosition, 
+                                        offsetTargetPosition, 
+                                        MaxSpeed, 
+                                        ArriveRadius,
+                                        Mass);
         _velocity = MoveAndSlide(_velocity);
         Triangle.Rotation = _velocity.Angle();
     }
